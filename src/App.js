3de15dae745f3738fa1dd16999af971f8c47f3bc
Rustem Arslanov <sobakasobaka6@gmail.com>
2021-10-09 16:07:10 +0500

    added build folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
+import {Text} from 'react';
+import './styles/App.css';
+
+/* 
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+*/
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="ControlDiv">
+        <h1 className="TextHeader">This is control</h1>
+      </div>
+      <div className="ContentDiv">
+        <h1 className="TextHeader">This is Content</h1>
+      </div>
     </div>
   );
 }
